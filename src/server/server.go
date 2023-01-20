355fe1b6af51c66b6bcb2ee8b12456388d4116d5
Kevin <kevschool1179@gmail.com>
2023-01-20 10:02:12 -0500

    Added hello world HTML code and basic Golang server.

---
+++ src/server/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func getRequest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, "Get Request Test")
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("."))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/get", getRequest)
+
+	fmt.Printf("Starting server at port 3000\n")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
