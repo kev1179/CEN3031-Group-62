c1930c805eb590bdc774a65fcbcd7870a1048d72
Kevin <kevschool1179@gmail.com>
2023-02-17 12:35:43 -0500

    Tried writing a sample request sending a GET request. Logic seems good, but the request is not reaching our server for some reason.

--- src/server/server_test.go
+++ src/server/server_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 
@@ -16,6 +21,33 @@ func TestSample(t *testing.T) {
 
 	got := add(11, 7)
 	want := 18
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestGetRequest(t *testing.T) {
+
+	/*
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			log.Fatal(err)
+		}
+	*/
+	resp, err := http.Get("http:localhost:8080/getTest")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//Convert the body to type string
+	sb := string(body)
+	log.Printf(sb)
+	got := sb
+	want := "Hello :)"
 
 	if got != want {
 		t.Errorf("got %q, wanted %q", got, want)
