bc33469d7a3df9b150cfa77131dd39d3869a0575
Kevin <kevschool1179@gmail.com>
2023-02-18 14:14:04 -0500

    Created system for user comments. Also wrote detailed comments for our code.

--- src/server/server.go
+++ src/server/server.go
@@ -7,11 +7,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
+// Class to represent user data. This will have to be expanded as the site grows.
 type User struct {
 	Firstname string
 	Lastname  string
@@ -20,10 +22,21 @@ type User struct {
 	Email     string
 }
 
+// Keeps track of the active user in the browsing session. This may need to be removed as I think a cookie based approach to this problem is better
+// https://www.sohamkamani.com/golang/session-cookie-authentication/
 type ActiveUser struct {
 	user User
 }
 
+// Keeps track of data when user makes a comment. We need to figure out how to seperate the comments by page.
+type Comment struct {
+	Username string
+	Time     string
+	Message  string
+	Page     string
+}
+
+// Registers a new user into the database.
 func registerHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
@@ -50,6 +63,7 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Determines if a login attempt was successful.
 func (activeUser ActiveUser) loginHandler(w http.ResponseWriter, r *http.Request) {
 
 	if err := r.ParseForm(); err != nil {
@@ -83,11 +97,13 @@ func (activeUser ActiveUser) loginHandler(w http.ResponseWriter, r *http.Request
 	http.Redirect(w, r, "http://localhost:4200/about", 301)
 }
 
+// Sample get request for front-end team to try
 func getRequestTest(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello :)")
 
 }
 
+// Sends the current user over to the front end team. As mentioned before we may need to do this completely differently.
 func getActiveUsername(w http.ResponseWriter, r *http.Request) {
 
 	jsonFile, err := os.Open("activeuser.json")
@@ -102,6 +118,28 @@ func getActiveUsername(w http.ResponseWriter, r *http.Request) {
 	defer jsonFile.Close()
 }
 
+// Puts a comment into the database when user creates a comment.
+func commentHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	fmt.Println(w, "POST request successful")
+	commentMessage := r.FormValue("comment")
+	currentTime := time.Now()
+	db, err := gorm.Open(sqlite.Open("comments.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+	//Username should be activeuser making the comment. Page should be supplied by front end when making post request.
+	comment := Comment{Username: "Bob", Time: currentTime.Format("01-02-2006 15:04:05"), Message: commentMessage, Page: "Food"}
+	db.Create(&comment)
+
+}
+
+// Test to make sure GO server is working properly
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
@@ -116,10 +154,12 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
+// IGNORE: for unit testing setup
 func add(x, y int) (res int) {
 	return x + y
 }
 
+// Starts server and sets URL's front-end can send requests to
 func main() {
 	var user User
 	fileServer := http.FileServer(http.Dir("."))
@@ -129,6 +169,7 @@ func main() {
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/getTest", getRequestTest)
 	http.HandleFunc("/getUsername", getActiveUsername)
+	http.HandleFunc("/postComment", commentHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
