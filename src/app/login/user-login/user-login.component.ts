546c6047678946b99f8ffffe1b1ea003074e5f7c
Patrick Quinlan <patrickquinlan6@gmail.com>
2023-02-27 12:56:20 -0500

    redesigned login and registration page and added input validation to login page to make sure that the form being sent has been properly filled. still need to tie into backend to see if the username and password match with existing users

--- src/app/login/user-login/user-login.component.ts
+++ src/app/login/user-login/user-login.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup} from '@angular/forms'
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css']
 })
-export class UserLoginComponent {
-public loginForm!: FormGroup
 
+export class UserLoginComponent {
+  loginForm!: FormGroup;
+  
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  Login() {
+    if(this.loginForm.valid) {
+      //sends user data to database
+      console.log(this.loginForm.value)
+    } else {
+      //throws error
+      console.log("Username or Password is incorrect");
+      this.validateForm(this.loginForm);
+      alert("invalid form");
+    }
+  }
+
+  private validateForm(formGroup:FormGroup) {
+    Object.keys(formGroup.controls).forEach(field=>{
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsDirty({onlySelf:true})
+      } else if (control instanceof FormGroup) {
+        this.validateForm(control)
+      }
+    })
+  }
 }
+
+  
--- src/app/user-registration/user-registration/user-registration.component.spec.ts
+++ src/app/user-registration/user-registration/user-registration.component.spec.ts
@@ -18,6 +18,6 @@ describe('UserRegistrationComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruth();
   });
 });
