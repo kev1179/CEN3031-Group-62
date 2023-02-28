8a60fa4278e8a0fa385801dec9c0a1f0d69035e5
Audrey Weigel <41082966+audreyweigel@users.noreply.github.com>
2023-02-28 17:01:15 -0500

    unit tests that don't work

--- src/app/about/about/about.component.spec.ts
+++ src/app/about/about/about.component.spec.ts
@@ -1,22 +1,29 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AboutComponent } from './about.component';
 
-import { AboutComponent } from "./about.component";
+describe('AboutComponent', () => {
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
 
-describe("AboutComponent", () => {
-    let component: AboutComponent;
-    let fixture: ComponentFixture<AboutComponent>;
-    
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-        declarations: [AboutComponent],
-        }).compileComponents();
-    
-        fixture = TestBed.createComponent(AboutComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
-    
-    it("should create", () => {
-        expect(component).toBeTruthy();
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AboutComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AboutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the AboutComponent', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display the title "About"', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('About');
+  });
 });
