159ed8076b909ca9c8b1da2951b622eba2b1835f
kev1179 <kevschool1179@gmail.com>
2023-03-28 14:06:40 -0400

    Saved changes

--- src/app/login/user-login/user-login.component.spec.ts
+++ src/app/login/user-login/user-login.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+<<<<<<< HEAD
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+=======
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AuthService } from 'src/app/services/auth.service';
+>>>>>>> 06ca4f0 (Saved changes)
 import { UserLoginComponent } from './user-login.component';
 
 describe('UserLoginComponent', () => {
@@ -11,7 +16,12 @@ describe('UserLoginComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ UserLoginComponent ],
+<<<<<<< HEAD
       imports: [ ReactiveFormsModule, HttpClientTestingModule ]
+=======
+      imports: [ ReactiveFormsModule, HttpClientTestingModule ],
+      providers: [ AuthService ]
+>>>>>>> 06ca4f0 (Saved changes)
     })
     .compileComponents();
   }));
@@ -23,10 +33,13 @@ describe('UserLoginComponent', () => {
     httpTestingController = TestBed.inject(HttpTestingController);
   });
 
+<<<<<<< HEAD
   afterEach(() => {
     httpTestingController.verify();
   });
 
+=======
+>>>>>>> 06ca4f0 (Saved changes)
   it('should create', () => {
     expect(component).toBeTruthy();
   });
@@ -42,12 +55,17 @@ describe('UserLoginComponent', () => {
     expect(window.alert).toHaveBeenCalledWith('Username or password is incorrect');
   });
 
+<<<<<<< HEAD
   /* it('should submit valid form', () => {
+=======
+  it('should submit valid form', () => {
+>>>>>>> 06ca4f0 (Saved changes)
     component.loginForm.setValue({
       username: 'john',
       password: 'password'
     });
     expect(component.loginForm.valid).toBeTruthy();
+<<<<<<< HEAD
     spyOn(component, 'Login').and.callThrough();
     spyOn(window, 'alert');
     component.Login();
@@ -58,3 +76,15 @@ describe('UserLoginComponent', () => {
     expect(window.alert).toHaveBeenCalledWith('success message from server');
   }); */
 });
+=======
+    spyOn(component.auth, 'login').and.callThrough();
+    spyOn(window, 'alert');
+    component.Login();
+    expect(component.auth.login).toHaveBeenCalledWith({
+      username: 'john',
+      password: 'password'
+    });
+    expect(window.alert).toHaveBeenCalledWith('success message from server');
+  });
+});
+>>>>>>> 06ca4f0 (Saved changes)
