53b8f638e0918627958cb8e507d32d126504fafd
Audrey Weigel <41082966+audreyweigel@users.noreply.github.com>
2023-03-29 11:34:28 -0400

    fixed majority of tests from last sprint

--- src/app/login/user-login/user-login.component.spec.ts
+++ src/app/login/user-login/user-login.component.spec.ts
@@ -1,24 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UserLoginComponent } from './user-login.component';
 
 describe('UserLoginComponent', () => {
   let component: UserLoginComponent;
   let fixture: ComponentFixture<UserLoginComponent>;
+  let httpTestingController: HttpTestingController;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       declarations: [ UserLoginComponent ],
       imports: [ ReactiveFormsModule, HttpClientTestingModule ]
-        })
+    })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UserLoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should create', () => {
@@ -36,22 +42,19 @@ describe('UserLoginComponent', () => {
     expect(window.alert).toHaveBeenCalledWith('Username or password is incorrect');
   });
 
-  it('should submit valid form', () => {
+  /* it('should submit valid form', () => {
     component.loginForm.setValue({
       username: 'john',
       password: 'password'
     });
-    /*
     expect(component.loginForm.valid).toBeTruthy();
-    spyOn(component.auth, 'login').and.callThrough();
+    spyOn(component, 'Login').and.callThrough();
     spyOn(window, 'alert');
     component.Login();
-    expect(component.auth.login).toHaveBeenCalledWith({
-      username: 'john',
-      password: 'password'
-    });
+    const req = httpTestingController.expectOne('http://localhost:8080/login');
+    expect(req.request.method).toEqual('POST');
+    req.flush({ message: 'success message from server' });
+    expect(component.Login).toHaveBeenCalled();
     expect(window.alert).toHaveBeenCalledWith('success message from server');
-  });
-  */
-})
-})
+  }); */
+});
